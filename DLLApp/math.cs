94248b12892b0fc01a02d46d3fbc6e8f695a944a
Preenit <80442475+Raj-boop-max@users.noreply.github.com>
2022-08-08 19:00:28 +1000

    Add files via upload

---
+++ DLLApp/math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLApp
+{
+    public static class math
+    {
+        public static String Word(String word) {
+
+            return word;
+        }
+    }
+}
