624c57ef46233d49bf7054601a811736b759eb26
Preenit <80442475+Raj-boop-max@users.noreply.github.com>
2022-08-08 19:02:50 +1000

    VS project file

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DLLApp;
+using DLLApp2;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+
+        {
+            String word = math.Word("Greatness!");
+            String word2 = Class1.Word2("Comes with hardwork!");
+            String result = word + "--" + word2;
+            lblHelloWorld.Text = result;
+        }
+
+        
+    }
+}
