28ab45a2b5b98251580af8cf66132e39fd5b5e72
Preenit <80442475+Raj-boop-max@users.noreply.github.com>
2022-08-08 19:01:30 +1000

    DLL file 2

---
+++ DLLApp2/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLApp2
+{
+    public static class Class1
+    {
+        public static String Word2(String word)
+        {
+
+            return word;
+        }
+    }
+}
